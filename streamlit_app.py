8ab7316af735ae0f01a50aa25ea12db7333757e3
chrisasesoruni <107228524+chrisasesoruni@users.noreply.github.com>
2022-06-09 20:45:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,9 @@
+# app.py, run with 'streamlit run app.py'
+import pandas as pd
+import streamlit as st
 
+df = pd.read_csv("https://www.datosabiertos.gob.pe/sites/default/files/Monitoreo_julio.xlsx")  # read a CSV file inside the 'data" folder next to 'app.py'
+# df = pd.read_excel(...)  # will work for Excel files
+
+st.title("Hello world!")  # add a title
+st.write(df)  # visualize my dataframe in the Streamlit app
