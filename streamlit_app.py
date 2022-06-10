0331e47b93b704b79038116a13b877dfd33a1688
chrisasesoruni <107228524+chrisasesoruni@users.noreply.github.com>
2022-06-09 20:51:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import streamlit as st
 
-df = pd.read_csv(r'https://www.datosabiertos.gob.pe/sites/default/files/Monitoreo_julio.xlsx', header= 0, encoding= 'unicode_escape')  # read a CSV file inside the 'data" folder next to 'app.py'
+df = pd.read_excel(r'https://www.datosabiertos.gob.pe/sites/default/files/Monitoreo_julio.xlsx', header= 0, encoding= 'unicode_escape')  # read a CSV file inside the 'data" folder next to 'app.py'
 # df = pd.read_excel(...)  # will work for Excel files
 
 st.title("Hello world!")  # add a title
