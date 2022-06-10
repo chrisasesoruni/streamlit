49f52a648073d966a5e667d682cd4aed709e2a41
chrisasesoruni <107228524+chrisasesoruni@users.noreply.github.com>
2022-06-09 20:53:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # app.py, run with 'streamlit run app.py'
 import pandas as pd
 import streamlit as st
+import openpyxl
 
 df = pd.read_excel(r'https://www.datosabiertos.gob.pe/sites/default/files/Monitoreo_julio.xlsx', header= 0)  # read a CSV file inside the 'data" folder next to 'app.py'
 # df = pd.read_excel(...)  # will work for Excel files
