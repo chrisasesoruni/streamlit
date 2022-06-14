5b2b14018fceb2a4d69d1132b1574946999f5a05
chrisasesoruni <107228524+chrisasesoruni@users.noreply.github.com>
2022-06-14 12:33:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ df_miraflores= pd.read_excel(r'https://www.datosabiertos.gob.pe/sites/default/fi
 st.title("Tabla Bonilla")  # add a title
 st.write(df_miraflores.dtypes.astype(str))
 #df_bonilla.iloc[:, 6:14].hist()
-st.line_chart(pd.concat([df_bonilla.iloc[:, 6],df_miraflores.iloc[:, 6]]))
+st.line_chart(pd.concat([df_bonilla.iloc[:, 6],df_miraflores.iloc[:, 6]], axis = 1))
 #plt.show()
 #st.pyplot()
 #st.write()  # visualize my dataframe in the Streamlit app
