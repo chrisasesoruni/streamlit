1da6376317d9c52dab378e3e41dcd3949d821b92
chrisasesoruni <107228524+chrisasesoruni@users.noreply.github.com>
2022-06-13 21:50:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,17 @@ import pandas as pd
 import streamlit as st
 import pip
 pip.main(["install", "openpyxl"])
+import plotly.figure_factory as ff
+import matplotlib.pyplot as plt
 
 df_bonilla = pd.read_excel(r'https://www.datosabiertos.gob.pe/sites/default/files/Monitoreo_setiembre_Bonilla.xlsx', header= 0) 
 df_miraflores= pd.read_excel(r'https://www.datosabiertos.gob.pe/sites/default/files/Monitoreo_setiembre_Ov.Miraflores.xlsx', header= 0) 
 # df = pd.read_excel(...)  # will work for Excel files
 
 st.title("Tabla Bonilla")  # add a title
-st.write(df_bonilla[:,6,14].hist())  # visualize my dataframe in the Streamlit app
+df_bonilla[:,6,14].hist()
+plt.show()
+st.pyplot()
+#st.write()  # visualize my dataframe in the Streamlit app
 st.title("Tabla Miraflores")  # add a title
 st.write(df_miraflores)  # visualize my dataframe in the Streamlit app
